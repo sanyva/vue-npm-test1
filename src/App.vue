8b4f4094ee8a03ca8d7a11b08093c05a0e251af7
Sanyva <sanyva@gmail.com>
2022-10-21 17:54:52 +0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 4"/>
+    <HelloWorld msg="Welcome to Your Vue.js App v2"/>
   </div>
 </template>
 
